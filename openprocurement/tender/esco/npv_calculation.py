8e6366667754198e684f0402e7dfd969823370b4
andrii-shchudlo <andriis@quintagroup.org>
2017-08-18 12:34:41 +0300

    Calculatate discounted income

--- openprocurement/tender/esco/npv_calculation.py
+++ openprocurement/tender/esco/npv_calculation.py
@@ -46,6 +46,15 @@ def calculate_discount_rates(
     ]
 
 
+def calculate_discounted_income(coef_discount, income_customer):
+    count = 0
+    discounted_income = []
+    for i in coef_discount:
+        discounted_income.append(i*income_customer[count])
+        count += 1
+    return discounted_income
+
+  
 def calculate_days_with_cost_reduction(
         announcement_date,
         days_per_year=DAYS_PER_YEAR,
